cccef1ce83e27752e9fe33e49c7d56d372014965
kimihiqq <kimihiqq@gmail.com>
2023-10-18 22:13:24 +0900

    feat: todo 서비스 및 리파지터리 생성

---
+++ til-spring-app/src/main/java/me/kimihiqq/tilspringapp/persistence/TodoRepository.java
@@ -0,0 +1,19 @@
+package me.kimihiqq.tilspringapp.persistence;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import me.kimihiqq.tilspringapp.model.TodoEntity;
+
+@Repository
+public interface TodoRepository extends JpaRepository<TodoEntity, String> {
+	List<TodoEntity> findByUserId(String userId);
+
+	@Query("SELECT t FROM TodoEntity t WHERE t.userId = ?1")
+	TodoEntity findByUserIdQuery(String userId);
+
+}
+
