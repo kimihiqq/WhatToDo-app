3f2b73c580ce447200f2ac079738f9e78be434e5
kimihiqq <kimihiqq@gmail.com>
2023-10-18 22:13:00 +0900

    feat: todo엔티티 생성 및 응답/반환 DTO 추가

---
+++ til-spring-app/src/main/java/me/kimihiqq/tilspringapp/dto/TestRequestBodyDTO.java
@@ -0,0 +1,10 @@
+package me.kimihiqq.tilspringapp.dto;
+
+import lombok.Data;
+
+@Data
+public class TestRequestBodyDTO {
+	private int id;
+	private String message;
+}
+
---
+++ til-spring-app/src/main/java/me/kimihiqq/tilspringapp/dto/TodoDTO.java
@@ -0,0 +1,33 @@
+package me.kimihiqq.tilspringapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import me.kimihiqq.tilspringapp.model.TodoEntity;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class TodoDTO {
+	private String id;
+	private String title;
+	private boolean done;
+
+	public TodoDTO(final TodoEntity entity) {
+		this.id = entity.getId();
+		this.title = entity.getTitle();
+		this.done = entity.isDone();
+	}
+
+	public static TodoEntity toEntity(final TodoDTO dto) {
+		return TodoEntity.builder()
+			.id(dto.getId())
+			.title(dto.getTitle())
+			.done(dto.isDone())
+			.build();
+	}
+
+}
+
