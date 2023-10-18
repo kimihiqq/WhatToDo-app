c311580a8f92a6f4eea723bb525286c85a937292
kimihiqq <kimihiqq@gmail.com>
2023-10-18 22:11:41 +0900

    feat: til-spring-app 초기화

---
+++ til-spring-app/src/main/java/me/kimihiqq/tilspringapp/TilSpringApp.java
@@ -0,0 +1,14 @@
+package me.kimihiqq.tilspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TilSpringApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TilSpringApp.class, args);
+	}
+
+}
+
---
+++ til-spring-app/src/test/java/me/kimihiqq/tilspringapp/TilSpringAppTests.java
@@ -0,0 +1,13 @@
+package me.kimihiqq.tilspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TilSpringAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
