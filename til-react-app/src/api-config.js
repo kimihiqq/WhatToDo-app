c2716e6b2cf7db218353e8ab5feb035185859f56
kimihiqq <kimihiqq@gmail.com>
2023-10-23 02:59:25 +0900

    feat: 배포 설정 추가

--- til-react-app/src/api-config.js
+++ til-react-app/src/api-config.js
@@ -5,7 +5,7 @@ const hostname = window && window.location && window.location.hostname;
 if (hostname === "localhost") {
   backendHost = "http://localhost:8080";
 } else {
-  backendHost = "http://prod-til-api-service.ap-northeast-2.elasticbeanstalk.com";
+  backendHost = "https://api.offonoff.net";
 }
 
 export const API_BASE_URL = `${backendHost}`;
